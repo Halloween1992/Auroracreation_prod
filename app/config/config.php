0e66ffc8d8c7b4618a22931e58c8b879d66fa367
M.Anwary <mujtaba.anwary1992@gmail.com>
2022-03-22 14:52:21 +0100

    updated

---
+++ app/config/config.php
@@ -0,0 +1,26 @@
+<?php
+  // DB Params
+
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '123456');
+// define('DB_NAME', 'alimedia_wp58906');
+
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'dorehami_aurora');
+define('DB_PASS', 'Untankidps1ap');
+define('DB_NAME', 'dorehami_aurora');
+
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+
+  // URL Root
+define('URLROOT', 'https://aurora.dorehami.se');
+
+  // URL Root
+  // define('URLROOT', 'http://localhost/shareposts');
+  // Site Name
+  define('SITENAME', 'AuroraPosts');
+  
---
+++ app/views/pages/index.php
@@ -0,0 +1,21 @@
+
+
+<?php require APPROOT . '/views/inc/header.php' ?>
+<div class="h-100 p-5 bg-light border rounded-3 my-4 ">
+    <div class="container">
+    <h1 class="text-center mb-4" > <?php echo $data['title'];?> </h1>
+    <p class="lead">
+    Simple blog posts build on the Sayed MVC PHP framwork, for Auroracreation recruitment test.
+    </p>
+    <p class="lead">
+    To see all posts you need to log in or create an account.
+    </p>
+    <p class="lead">
+    All of the source codes are saved in Github
+here is the <a href="https://github.com/Halloween1992/Auroracreation.git" target="blank">link </a> to check.
+    </p>
+    </div>
+</div>
+
+
+<?php require APPROOT . '/views/inc/footer.php' ?>
